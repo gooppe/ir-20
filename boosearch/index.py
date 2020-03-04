c11dbd0091d945cc23158dead0668749fafed77e
Michael <mis5530@yandex.ru>
2020-03-04 17:24:47 +0300

    added indexation teamplate

---
+++ boosearch/index.py
@@ -0,0 +1,7 @@
+from typing import Tuple, List, Hashable, Dict
+
+
+def block_indexation(
+    block: List[Tuple[Hashable, List[str]]]
+) -> Dict[str, List[Hashable]]:
+    raise NotImplementedError
