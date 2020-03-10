1bfbe70f6b0aa844ea6ba33ced311a461200547f
Michael <mis5530@yandex.ru>
2020-03-10 23:13:40 +0300

    added global indexation

--- boosearch/index.py
+++ boosearch/index.py
@@ -1,26 +1,82 @@
-from typing import Tuple, List, Hashable, Dict
+from typing import Tuple, List, Hashable, Dict, Iterable, Generator
 from collections import defaultdict
-import pickle
+from itertools import islice, chain
+import json
+import tempfile
+import os
 
 
-def block_indexation(
-    block: List[Tuple[Hashable, List[str]]]
-) -> Dict[str, List[Hashable]]:
-    reversed_index = defaultdict(list)
+def batchify(data: Iterable, buffer_size: int) -> Generator:
+    data_iter = iter(data)
+    while True:
+        batch_iter = islice(data_iter, buffer_size)
+        yield chain([next(batch_iter)], batch_iter)
+
+
+def build_index(
+    data: Iterable[Tuple[int, List[str]]], filename: str, buffer_size: int = 10
+):
+    """
+
+    Args:
+        data:
+        filename:
+        buffer_size:
+
+    Returns:
+
+    """
+    # save data by blocks
+
+    temp_dir = tempfile.gettempdir()
+    counter = 0
+    for i, block in enumerate(batchify(data, buffer_size)):
+        counter += 1
+        temp_block = sub_block_indexation(block)
+        temp_filename = os.path.join(temp_dir, f"temp_block_{i}")
+        dump_data(temp_block, temp_filename)
+
+    # load data
+
+    concatenate_data(counter)
+
+    return True
+
+
+def sub_block_indexation(
+    block: Iterable[Tuple[Hashable, List[str]]]
+) -> List[Tuple[str, List[int]]]:
+
+    reversed_index_dict = defaultdict(list)
+    reversed_index_list = []
+
     for doc_id, tokens in block:
         for term in tokens:
-            if doc_id not in reversed_index[term]:
-                reversed_index[term].append(doc_id)
-    return reversed_index
+            if doc_id not in reversed_index_dict[term]:
+                reversed_index_dict[term].append(doc_id)
+
+    list_keys = list(reversed_index_dict.keys())
+    list_keys.sort()
+
+    for keys in list_keys:
+        reversed_index_list.append((keys, sorted(reversed_index_dict[keys])))
+
+    return reversed_index_list
 
 
-def dump_data(data: Dict[str, List[Hashable]], filename: str) -> bool:
-    with open(filename, "wb") as f:
-        pickle.dump(data, f)
+def concatenate_data(number_of_files: int):
+    raise NotImplementedError
+
+
+def dump_data(data: List[Tuple[str, List[int]]], filename: str) -> bool:
+    with open(filename, "w") as f:
+        for d in data:
+            dumps = json.dumps(d)
+            print(dumps, file=f)
     return True
 
 
 def load_data(filename: str) -> Dict[str, List[Hashable]]:
     with open(filename, "rb") as f:
-        data = pickle.load(f)
+        data = json.load(f)
     return data
--- tests/test_index.py
+++ tests/test_index.py
@@ -15,29 +15,16 @@ def test_block_indexation():
         "ты": [2, 3],
         "алгебра": [2],
     }
-    temp = index.block_indexation(test_list)
+    temp = index.sub_block_indexation(test_list)
     assert temp == result_dict
 
 
-def test_dump_data():
-    test_dict = {
-        "я": [1, 3],
-        "любить": [1, 2, 3],
-        "инфопоиск": [1],
-        "ты": [2, 3],
-        "алгебра": [2],
-    }
-    result = index.dump_data(test_dict, "test.pickle")
-    assert True == result
-
-
-def test_load_data():
-    test_dict = {
-        "я": [1, 3],
-        "любить": [1, 2, 3],
-        "инфопоиск": [1],
-        "ты": [2, 3],
-        "алгебра": [2],
-    }
-    result = index.load_data("test.pickle")
-    assert result == test_dict
+def test_build_index():
+    test_list = [
+        (1, ["я", "любить", "инфопоиск"]),
+        (2, ["ты", "любить", "алгебра"]),
+        (3, ["я", "любить", "ты", "я"]),
+        (4, []),
+    ]
+    result = index.build_index(test_list, 1)
+    assert result
