4083cfdedcc52af32e1ab1229f44914f2fa4f6f9
Michael <mis5530@yandex.ru>
2020-03-10 20:56:25 +0300

    forgotten files from previous commit

--- boosearch/index.py
+++ boosearch/index.py
@@ -1,7 +1,26 @@
 from typing import Tuple, List, Hashable, Dict
+from collections import defaultdict
+import pickle
 
 
 def block_indexation(
     block: List[Tuple[Hashable, List[str]]]
 ) -> Dict[str, List[Hashable]]:
-    raise NotImplementedError
+    reversed_index = defaultdict(list)
+    for doc_id, tokens in block:
+        for term in tokens:
+            if doc_id not in reversed_index[term]:
+                reversed_index[term].append(doc_id)
+    return reversed_index
+
+
+def dump_data(data: Dict[str, List[Hashable]], filename: str) -> bool:
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+    return True
+
+
+def load_data(filename: str) -> Dict[str, List[Hashable]]:
+    with open(filename, "rb") as f:
+        data = pickle.load(f)
+    return data
