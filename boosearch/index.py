4931f301ed3207923685208cc19086e2248bde1a
gooppe <gooppe@yandex.ru>
2020-03-13 11:20:53 +0300

    Added docs

--- boosearch/index.py
+++ boosearch/index.py
@@ -14,6 +14,14 @@ def _iter_json(filename: str) -> Iterable[Any]:
 def index_json(
     filename: str, index_name: str, target_collumn: int, buffer_size: int = 10000
 ):
+    """Start indexing json file.
+
+    Args:
+        filename (str): name of source file.
+        index_name (str): name of output index file.
+        target_collumn (int): index of the processed column of json.
+        buffer_size (int, optional): default buffer size. Defaults to 10000.
+    """
     stopwords = load_stopwords("ru")
 
     data = _iter_json(filename)
