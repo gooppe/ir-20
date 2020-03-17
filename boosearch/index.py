a79527f82386a9b6deaaea56cd7c98d396e20594
Michael <mis5530@yandex.ru>
2020-03-17 19:29:41 +0300

    final index merge

--- boosearch/index.py
+++ boosearch/index.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Hashable, Dict, Iterable, Generator
+from typing import Tuple, List, Hashable, Dict, Iterable, Generator, IO, Any
 from collections import defaultdict
 from itertools import islice, chain
 import json
@@ -13,19 +13,16 @@ def batchify(data: Iterable, buffer_size: int) -> Generator:
         yield chain([next(batch_iter)], batch_iter)
 
 
+def list_merge(lstlst: List[List[int]]) -> List[int]:
+    data = []
+    for lst in lstlst:
+        data.extend(lst)
+    return data
+
+
 def build_index(
     data: Iterable[Tuple[int, List[str]]], filename: str, buffer_size: int = 10
 ):
-    """
-
-    Args:
-        data:
-        filename:
-        buffer_size:
-
-    Returns:
-
-    """
     # save data by blocks
 
     temp_dir = tempfile.gettempdir()
@@ -33,12 +30,11 @@ def build_index(
     for i, block in enumerate(batchify(data, buffer_size)):
         counter += 1
         temp_block = sub_block_indexation(block)
-        temp_filename = os.path.join(temp_dir, f"temp_block_{i}")
+        temp_filename = os.path.join(temp_dir, f"temp_block_{i}.txt")
         dump_data(temp_block, temp_filename)
 
     # load data
-
-    concatenate_data(counter)
+    concatenate_data(counter, filename)
 
     return True
 
@@ -64,19 +60,105 @@ def sub_block_indexation(
     return reversed_index_list
 
 
-def concatenate_data(number_of_files: int):
-    raise NotImplementedError
+def concatenate_data(number_of_files: int, filename: str):
+
+    pointers = load_file_pointers(number_of_files)
+    # маска переходов по файлам (один раз мы должны прочитать вообще все файлы)
+    mask = [True] * number_of_files
+    min_storage = []  # хранилище doc_id минимального терма
+    min_indexes = []  # индексы минимального терма для изменения маски
+    min_term = ""  # хранилище минимального терма
+    term = ""  # инициализация локальных переменных для красоты
+    docs = []
+
+    main_file = open(filename, "w")
+
+    buffer_docs = (
+        []
+    )  # буферные листы для термов и их doc_id которые мы не использовали на итерации
+    buffer_terms = []
+    eof_list = [False] * number_of_files  # флаг концов файла
+
+    for i in range(
+        0, number_of_files
+    ):  # инициализация длины буфера (по количеству файлов)
+        buffer_docs.append([])
+        buffer_terms.append("")
+
+    while True:
+        min_storage.clear()
+        min_indexes.clear()
+        min_term = ""
+        term = ""
+        docs = []
+
+        for i in range(0, number_of_files):
+            if eof_list[i]:  # если файл закончился, то не читаем
+                continue
+
+            if mask[i]:
+                try:
+                    # если мы должны прочитать файл, то мы меняем его буфер
+                    line = next(pointers[i])
+                    term, docs = json.loads(line)
+                    buffer_docs[i] = docs
+                    buffer_terms[i] = term
+                except StopIteration:
+                    eof_list[i] = True
+                    continue
+
+            # если же файл не прочитан, то мы загружаем его предыдущее состояние
+            else:
+                term = buffer_terms[i]
+                docs = buffer_docs[i]
+
+            # если терм пустой, значит мы в самом начале, когда ещё ничего не было подгружено
+            if min_term == "":
+                min_term = term
+                min_storage.append(docs)
+                min_indexes.append(i)
+            else:
+                # если термы равны, докидываем лист с doc_id и индекс
+                if term == min_term:
+                    min_storage.append(docs)
+                    min_indexes.append(i)
+                else:
+                    # если нашли терм поменьше, скидываем всё накопленное и заново строим листы
+                    if term < min_term:
+                        min_storage.clear()
+                        min_indexes.clear()
+                        min_term = term
+                        min_storage.append(docs)
+                        min_indexes.append(i)
+
+        # пересоздали маску, чтобы читать только те файлы, где лежит минимум
+        mask = [False] * number_of_files
+        for i in min_indexes:
+            mask[i] = True
+
+        return_docs = list_merge(min_storage)  # сливаем списки, сортируем, устраиваем
+        return_docs.sort()
+        dump = json.dumps((min_term, return_docs), ensure_ascii=False)
+        print(dump, file=main_file)
+
+        if eof_list == [True] * number_of_files:
+            break
+
+    main_file.close()
 
 
 def dump_data(data: List[Tuple[str, List[int]]], filename: str) -> bool:
     with open(filename, "w") as f:
         for d in data:
-            dumps = json.dumps(d)
+            dumps = json.dumps(d, ensure_ascii=False)
             print(dumps, file=f)
     return True
 
 
-def load_data(filename: str) -> Dict[str, List[Hashable]]:
-    with open(filename, "rb") as f:
-        data = json.load(f)
-    return data
+def load_file_pointers(number_of_files: int) -> List[IO[Any]]:
+    pointers = []
+    tempdir = tempfile.gettempdir()
+    for i in range(0, number_of_files):
+        temp = open(os.path.join(tempdir, f"temp_block_{i}.txt"), "r")
+        pointers.append(temp)
+    return pointers
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,4 +1,6 @@
 from boosearch import index
+import tempfile
+import os
 
 
 def test_block_indexation():
@@ -23,8 +25,17 @@ def test_build_index():
     test_list = [
         (1, ["я", "любить", "инфопоиск"]),
         (2, ["ты", "любить", "алгебра"]),
-        (3, ["я", "любить", "ты", "я"]),
+        (3, ["я", "ходить", "ты", "я"]),
         (4, []),
+        (5, ["ты", "ненавидеть", "матанализ"]),
+        (6, ["мы", "ходить", "матмех"]),
+        (7, ["они", "любить", "ты", "я"]),
+        (8, ["они"]),
+        (9, ["я", "тащить", "каток"]),
+        (10, ["ты", "сдать", "механика"]),
+        (11, ["не", "любить", "не", "ты"]),
+        (12, ["кусать"]),
     ]
-    result = index.build_index(test_list, 1)
+    tempdir = tempfile.gettempdir()
+    result = index.build_index(test_list, os.path.join(tempdir, "final_file.txt"), 3)
     assert result
